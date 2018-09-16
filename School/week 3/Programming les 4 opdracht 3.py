b2a1979e196c55e3c6e573bbf166c8d3650bd6b7
Tobias <tobias.vandenhoogen@student.hu.nl>
2018-09-16 16:25:25 +0200

    Code van opdracht 3 van les 4 om te testen of Github werkt.

---
+++ School/week 3/Programming les 4 opdracht 3.py
@@ -0,0 +1,3 @@
+uurloon = float(input('Wat verdien je per uur: '))
+aantal_uur = int(input('Hoeveel uur heb je gewerkt: '))
+print(str(aantal_uur) + ' uur heb je gewerkt en dat levert ' + str(uurloon * aantal_uur) + ' euro op.' )
